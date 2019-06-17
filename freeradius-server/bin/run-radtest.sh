b63b59049bed077bda2dadd824f27a3a93881f5b
Jorge Pereira <jpereira@freeradius.org>
2019-06-17 18:44:15 -0300

    Update password

--- freeradius-server/bin/run-radtest.sh
+++ freeradius-server/bin/run-radtest.sh
@@ -1,12 +1,11 @@
+#!/bin/bash
 
-cat <<EOF | radclient -x 172.17.0.2:1812 auth testing123
-User-Name = "jorge"
-User-Password = "jorge"
+cat <<EOF | radclient -x 127.0.0.1:1812 auth testing123
+User-Name = "bob"
+User-Password = "hello"
+Cleartext-Password = "hello"
 NAS-IP-Address = 127.0.1.1
 NAS-Port = 1
 Message-Authenticator = 0x00
-Cleartext-Password = "jorge"
 ASA-IE-Proxy-PAC-URL = "${1:-caipirinha padrao}"
 EOF
-
-
