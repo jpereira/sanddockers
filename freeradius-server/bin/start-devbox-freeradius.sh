ad4e1fb4ad087202bd3c2b05aa88a251be5a59c7
Jorge Pereira <jpereira@freeradius.org>
2019-06-17 18:42:35 -0300

    Minor changes

--- freeradius-server/bin/start-devbox-freeradius.sh
+++ freeradius-server/bin/start-devbox-freeradius.sh
@@ -1,35 +1,37 @@
 #!/bin/bash
+# Author: Jorge Pereira <jpereira@freeradius.org>
+#
 
 export LC_ALL=C
-
-if [ "$1" = "centos" ]; then
-	image_os="centos7"
-	image_name="jpereira/build-centos7"
-else
-	image_os="ubuntu16"
-	image_name="jpereira/build-ubuntu16-deps"
-fi
-
-set +fx
 
 if [ -z "$SSH_AUTH_SOCK" ]; then
 	echo "ERRO: SSH_AUTH_SOCK ??? call ssh-agent"
 	exit 1
 fi
 
+if [ "$1" = "centos" ]; then
+	image_os="centos7"
+	image_name="jpereiran/sanddockers:devbox-freeradius-server-centos7"
+else
+	image_os="ubuntu16"
+	image_name="jpereiran/sanddockers:devbox-freeradius-server"
+fi
+
 docker_image="freeradius-$image_os"
+
+mkdir -p ~/Devel
+
+set +fx
 
 if ! docker ps -a --format  '{{.Names}}' | grep "^$docker_image$"; then
 	docker run --name="$docker_image" \
 			--privileged \
 			--cap-add SYS_PTRACE \
 			-h "docker-$docker_image" \
-			-v /home/$USER/Devel/:/root/Devel \
-			-v /opt/docker-rootfs/freeradius/:/opt/ \
+			-v ~/Devel/:/root/Devel \
 			-v $(dirname $SSH_AUTH_SOCK):$(dirname $SSH_AUTH_SOCK) \
 			-e LC_ALL=C \
 			-e SSH_AUTH_SOCK=$SSH_AUTH_SOCK \
-			-w /root/Devel/FreeRadius/freeradius-server.git \
 			-dti $image_name /bin/bash
 else
 	docker start "$docker_image"
