7c73ae82a2162aa4dd9ceda3cf5264181228b8db
Jorge Pereira <jpereira@freeradius.org>
2019-06-22 22:04:47 -0300

    Minor tweaks

--- freeradius-server/bin/start-devbox-freeradius.sh
+++ freeradius-server/bin/start-devbox-freeradius.sh
@@ -4,6 +4,8 @@
 
 export LC_ALL=C
 
+docker_rootfs="/opt/docker-rootfs/freeradius/"
+
 if [ -z "$SSH_AUTH_SOCK" ]; then
 	echo "ERRO: SSH_AUTH_SOCK ??? call ssh-agent"
 	exit 1
@@ -19,14 +21,17 @@ fi
 shift
 
 docker_image="devbox-$image_os"
-extra_opts="-v /opt/docker-rootfs/freeradius/:/opt"
 
 if [ "$1" = "reset" ]; then
 	echo "# Removing $docker_image (relaunch)"
 	docker rm -f $docker_image
 fi
 
-mkdir -p ~/Devel /opt/docker-rootfs/freeradius/
+mkdir -p ~/Devel $docker_rootfs
+
+if [ -d "$docker_rootfs" ]; then
+	extra_opts="-v $docker_rootfs:/opt"
+fi
 
 #set -fx
 if ! docker ps -a --format  '{{.Names}}' | grep "^$docker_image$"; then
@@ -35,7 +40,7 @@ if ! docker ps -a --format  '{{.Names}}' | grep "^$docker_image$"; then
 			--cap-add SYS_PTRACE \
 			-h "$docker_image" \
 			-v ~/Devel/:/root/Devel \
-			-w /root/Devel/FreeRadius/freeradius-server.git \
+			-w ~/Devel/FreeRadius/freeradius-server.git \
 			${extra_opts[*]} \
 			-v $(dirname $SSH_AUTH_SOCK):$(dirname $SSH_AUTH_SOCK) \
 			-e LC_ALL=C \
