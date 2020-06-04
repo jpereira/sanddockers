0b41a278b850b496b7be0c9eeec6b6cf726dff2f
Jorge Pereira <jpereiran@gmail.com>
2020-06-03 23:01:54 -0300

    Add x11 commands

--- freeradius-server/bin/start-devbox.sh
+++ freeradius-server/bin/start-devbox.sh
@@ -34,15 +34,26 @@ if [ -d "$docker_rootfs" ]; then
 	extra_opts="-v $docker_rootfs:/opt"
 fi
 
+if [ -n "$x11" ]; then
+	x11_args="-e DISPLAY=192.168.1.11:0"
+
+	echo "Starting - socat TCP-LISTEN:6000,reuseaddr,fork UNIX-CLIENT:\"$DISPLAY\""
+	killall -9 socat 1> /dev/null 2>&1
+	socat TCP-LISTEN:6000,reuseaddr,fork UNIX-CLIENT:\"$DISPLAY\" &
+
+	echo "Appending x11_args='$x11_args'"
+fi
+
 #set -fx
 if ! docker ps -a --format  '{{.Names}}' | grep "^$docker_image$"; then
 	docker run ${docker_opts[*]} --name="$docker_image" \
 			--privileged \
+			${x11_args[*]} \
 			--cap-add=ALL \
 			-p 1812:1812/udp -p 1813:1813/udp -p 3799:3799/udp \
 			-h "$docker_image" \
 			-v $HOME/Devel/:/root/Devel \
-			-w /root/Devel/FreeRADIUS/freeradius-server.git \
+			-w /root/Devel/FreeRADIUS/freeradius-server.git-linux \
 			${extra_opts[*]} \
 			-v $(dirname $SSH_AUTH_SOCK):$(dirname $SSH_AUTH_SOCK) \
 			-e LC_ALL=C \
