a5c6640af315bcd24e93d398c170cde823d48a1b
Jorge Pereira <jpereiran@gmail.com>
2020-06-26 15:26:46 -0300

    Fix typos

--- freeradius-server/bin/start-devbox.sh
+++ freeradius-server/bin/start-devbox.sh
@@ -6,6 +6,7 @@ export LC_ALL=C
 
 docker_rootfs="$HOME/Docker/storage/freeradius/"
 cwd="/root/Devel/FreeRADIUS/freeradius-server.git"
+arg0="$(basename $0)"
 
 if [ -z "$SSH_AUTH_SOCK" ]; then
 	echo "ERRO: SSH_AUTH_SOCK ??? call ssh-agent"
@@ -35,39 +36,52 @@ if [ -d "$docker_rootfs" ]; then
 	extra_opts="-v $docker_rootfs:/opt"
 fi
 
+#
+#	Settings for X11 forward
+#
 if [ -z "$nox11" ]; then
-	if [ "$(uname -s)" = "Linux" ]; then
-		ipaddr4="$(ifconfig docker0 | awk '/inet /{print $2}')"
-	else
-		ipaddr4="192.168.1.11"
-	fi
+	uname_s="$(uname -s)"
 
-	x11_args="-e DISPLAY=${ipaddr4}:0"
+	echo "${arg0}: [**] Setup X11 settings"
+	xhost + 1> /dev/null 2>&1
 
-	echo "Starting - socat TCP-LISTEN:6000,reuseaddr,fork UNIX-CLIENT:\"$DISPLAY\""
-	killall -9 socat 1> /dev/null 2>&1
-	socat TCP-LISTEN:6000,reuseaddr,fork UNIX-CLIENT:\"$DISPLAY\" &
+	case ${uname_s} in
+		Linux)
+			ipaddr4="$(ifconfig docker0 | awk '/inet /{ print $2}')"
+			cwd="${cwd}"
+			x11_args="-v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=${DISPLAY}"
+		;;
 
-	echo "Appending x11_args='$x11_args'"
+		Darwin)
+			ipaddr4="$(ifconfig en0 | awk '/inet /{ print $2}')"
+			cwd="${cwd}-linux"
+			x11_args="-e DISPLAY=${ipaddr4}:0"
+
+			echo "${arg0}: [**] Starting - socat TCP-LISTEN:6000,reuseaddr,fork UNIX-CLIENT:\"$DISPLAY\""
+			killall -9 socat 1> /dev/null 2>&1
+			socat TCP-LISTEN:6000,reuseaddr,fork "UNIX-CLIENT:\"$DISPLAY\"" &
+		;;
+	esac
+
+	echo "${arg0}: [**] Appending x11_args='$x11_args'"
 fi
-
-[ "$(uname -s)" != "Linux" ] && cwd="${cwd}-linux"
 
 #set -fx
 if ! docker ps -a --format  '{{.Names}}' | grep "^$docker_image$"; then
-	docker run ${docker_opts[*]} --name="$docker_image" \
-			--privileged \
-			${x11_args[*]} \
-			--cap-add=ALL \
-			-p 1812:1812/udp -p 1813:1813/udp -p 3799:3799/udp \
-			-h "$docker_image" \
-			-v $HOME/Devel/:/root/Devel \
-			-w "$cwd" \
-			${extra_opts[*]} \
+	docker run ${docker_opts[*]} --name="$docker_image"            \
+			--privileged                                           \
+			${x11_args[*]}                                         \
+			${extra_opts[*]}                                       \
+			--cap-add=ALL                                          \
+			-h "$docker_image"                                     \
+			-v $HOME/Devel/:/root/Devel                            \
+			-w "$cwd"                                              \
+			-e LC_ALL=C                                            \
+			-p 1812:1812/udp -p 1813:1813/udp -p 3799:3799/udp     \
 			-v $(dirname $SSH_AUTH_SOCK):$(dirname $SSH_AUTH_SOCK) \
-			-e LC_ALL=C \
-			-e SSH_AUTH_SOCK=$SSH_AUTH_SOCK \
-			-e SHELL=$SHELL -e COLUMNS=$COLUMNS -e LINES=$LINES -e TERM=$TERM \
+			-e SSH_AUTH_SOCK=$SSH_AUTH_SOCK                        \
+			-e SHELL=$SHELL -e COLUMNS=$COLUMNS                    \
+			-e LINES=$LINES -e TERM=$TERM                          \
 			-dti $image_name /bin/bash
 else
 	docker start "$docker_image"
