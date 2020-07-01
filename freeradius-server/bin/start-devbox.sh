f2c9d83a135311678b1234482e75bdf39e9e338c
Jorge Pereira <jpereiran@gmail.com>
2020-07-01 18:38:11 -0300

    Fix the start script

--- freeradius-server/bin/start-devbox.sh
+++ freeradius-server/bin/start-devbox.sh
@@ -2,40 +2,86 @@
 # Author: Jorge Pereira <jpereira@freeradius.org>
 #
 
-export LC_ALL=C
-
-docker_rootfs="$HOME/Docker/storage/freeradius/"
-cwd="/root/Devel/FreeRADIUS/freeradius-server.git"
-arg0="$(basename $0)"
-
 if [ -z "$SSH_AUTH_SOCK" ]; then
 	echo "ERRO: SSH_AUTH_SOCK ??? call ssh-agent"
 	exit 1
 fi
 
-if [ "$1" = "centos" ]; then
-	image_os="freeradius-server-centos7"
-#	image_name="jpereiran/devbox-freeradius-server:centos7"
-	image_name="freeradius-centos7"
-else
-	image_os="freeradius-server-ubuntu_2004"
-#	image_name="jpereiran/devbox-freeradius-server:latest"
-	image_name="jpereiran/devbox-freeradius-server:ubuntu_2004"
-fi
-shift
+export LC_ALL=C
+export SSH_AUTH_SOCK_PATH="$(dirname $SSH_AUTH_SOCK)"
 
+docker_rootfs="$HOME/Docker/storage/freeradius/"
+cwd="/root/Devel/FreeRADIUS/freeradius-server.git"
+arg0="$(basename $0)"
+
+image_exec="/bin/bash"
+image_os="freeradius-server-ubuntu_2004"
+image_name="jpereiran/devbox-freeradius-server:latest"
+#image_name="jpereiran/devbox-freeradius-server:ubuntu_2004"
 docker_image="devbox-$image_os"
 
-if [ "$1" = "reset" ]; then
-	echo "$arg0: [**] Removing $docker_image (relaunch)"
-	docker rm -f $docker_image
-fi
+#
+#	Default docker options
+#
+decho() {
+	echo "$arg0: [**] $@"
+}
 
-mkdir -p $docker_rootfs
+#
+#	Check opts
+#
+while getopts 'adrpnPhs' OPTION; do
+	case $OPTION in
+		a)
+			decho "Enabling --cap-add=NET_ADMIN"
+			extra_opts+="--cap-add=NET_ADMIN "
+		;;
 
-if [ -d "$docker_rootfs" ]; then
-	extra_opts="-v $docker_rootfs:/opt"
-fi
+		d)
+			decho "Enabling debug mode"
+			debug_on="set -fx"
+			debug_off="set +fx"
+		;;
+
+		r)
+			decho "Reset $docker_image container"
+			docker rm -f $docker_image 1> /dev/null 2>&1
+		;;
+
+		p)
+			decho "Enabling --pid=host"
+			extra_opts+="--pid=host "
+		;;
+
+		n)
+			decho "Enabling --net=host"
+			extra_opts+="--net=host "
+		;;
+
+		P)
+			decho "Enabling --privileged"
+			extra_opts+="--privileged "
+		;;
+
+		s)
+			decho "Starting 'screen -R'"
+			image_exec=($image_exec -c 'sleep 1; exec 1> /dev/tty 2> /dev/tty < /dev/tty && screen -R; exit 0')
+		;;
+
+		?|h)
+			echo "Usage: $0 [-arpnPsh]"
+			echo
+			echo "-a 	Start with NET_ADMIN"
+			echo "-r 	Relaunch a new instance"
+			echo "-p 	Enable --pid=host"
+			echo "-n 	Enable --net=host"
+			echo "-P 	Enable privileged: --privileged"
+			echo "-s 	Start with \"screen -RA\""
+			echo "-h 	Show help"
+
+			exit
+	esac
+done
 
 #
 #	Settings for X11 forward
@@ -43,7 +89,6 @@ fi
 if [ -z "$nox11" ]; then
 	uname_s="$(uname -s)"
 
-	echo "${arg0}: [**] Setup X11 settings"
 	xhost + 1> /dev/null 2>&1
 
 	case ${uname_s} in
@@ -58,38 +103,43 @@ if [ -z "$nox11" ]; then
 			cwd="${cwd}-linux"
 			x11_args="-e DISPLAY=${ipaddr4}:0"
 
-			echo "${arg0}: [**] Starting - socat TCP-LISTEN:6000,reuseaddr,fork UNIX-CLIENT:\"$DISPLAY\""
+			decho "Starting - socat TCP-LISTEN:6000,reuseaddr,fork UNIX-CLIENT:\"$DISPLAY\""
 			killall -9 socat 1> /dev/null 2>&1
 			socat TCP-LISTEN:6000,reuseaddr,fork "UNIX-CLIENT:\"$DISPLAY\"" &
 		;;
 	esac
 
-	echo "${arg0}: [**] Appending x11_args='$x11_args'"
+	decho "Setup X11 settings x11_args='$x11_args'"
 fi
 
-extra_opts="--net=host ${extra_opts}"
+mkdir -p $docker_rootfs
 
-#set -fx
-if ! docker ps -a --format  '{{.Names}}' | grep "^$docker_image$"; then
+if [ -d "$docker_rootfs" ]; then
+	extra_opts+=" -v $docker_rootfs:/opt"
+fi
+
+if ! docker ps -a --format  '{{.Names}}' | grep -q "^$docker_image$"; then
+	decho "Lauching new instance"
+	$debug_on
 	docker run ${docker_opts[*]} --name="$docker_image"            \
-			--privileged                                           \
 			${x11_args[*]}                                         \
 			${extra_opts[*]}                                       \
-			--cap-add=NET_ADMIN                                          \
 			-h "$docker_image"                                     \
-			-v $HOME/Devel/:/root/Devel                            \
+			-v "$HOME/Devel/:/root/Devel"                          \
 			-w "$cwd"                                              \
-			-e LC_ALL=C                                            \
+			-v "${SSH_AUTH_SOCK_PATH}:${SSH_AUTH_SOCK_PATH}"       \
+			-e "LC_ALL=C"                                          \
+			-e "SSH_AUTH_SOCK=$SSH_AUTH_SOCK"                      \
+			-e "SHELL=$SHELL"                                      \
+			-e "COLUMNS=$COLUMNS"                                  \
+			-e "LINES=$LINES"                                      \
+			-e "TERM=$TERM"                                        \
 			-p 1812:1812/udp -p 1813:1813/udp -p 3799:3799/udp     \
-			-v $(dirname $SSH_AUTH_SOCK):$(dirname $SSH_AUTH_SOCK) \
-			-e SSH_AUTH_SOCK=$SSH_AUTH_SOCK                        \
-			-e SHELL=$SHELL -e COLUMNS=$COLUMNS                    \
-			-e LINES=$LINES -e TERM=$TERM                          \
-			-dti $image_name /bin/bash
+			-dti "$image_name" "${image_exec[@]}"
+	$debug_off
 else
+	decho "Relaunch previous instance"
 	docker start "$docker_image"
 fi
 
 docker attach "$docker_image"
-
-#set +fx
